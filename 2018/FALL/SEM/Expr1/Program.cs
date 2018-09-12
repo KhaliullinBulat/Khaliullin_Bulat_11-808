24b1cffc5f3d97a6d0d55d1448b94cfeea0f8f8c
Khaliullin Bulat <incidentalname636@gmail.com>
2018-09-12 13:51:16 +0300

    [add] Expr1

---
+++ 2018/FALL/SEM/Expr1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Slide01
+{
+    class Program
+    { 
+        /// <summary>
+        /// Expr1.
+        /// Swap 2 variables without using the third one
+        /// </summaryg
+        static void Main()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+    }
+}
