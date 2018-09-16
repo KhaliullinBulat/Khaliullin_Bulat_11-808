ebbda7af73e08811ad464f9df366417dc72ee22f
Khaliullin Bulat <incidentalname636@gmail.com>
2018-09-16 18:43:09 +0300

    [add] Expr3

---
+++ 2018/FALL/SEM/Expr2/Practise.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Slide01
+{
+    /// <summary>
+    /// Replacing first and third numerals of three-digit number
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int firstNumeral = number / 100;
+            int secondNumeral = (number% 100) / 10;
+            int thirdNumeral = number % 10;
+            Console.WriteLine(thirdNumeral*100 + secondNumeral*10 + firstNumeral);
+            Console.ReadKey();
+        }
+    }
+}
