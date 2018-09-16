85bbd771ebee361cc670b98335b46e74489a8fc5
Khaliullin Bulat <incidentalname636@gmail.com>
2018-09-16 18:15:50 +0300

    [add] Expr3

---
+++ 2018/FALL/SEM/Expr3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Slide01
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            int hours = Convert.ToInt32(Console.ReadLine());
+            int n = hours % 6;
+            int degree;
+            if (hours % 12 >= 6)
+                degree = (6 - n) * 30;
+            else
+                degree = n * 30;
+            Console.WriteLine(degree);
+            Console.ReadKey();
+        }
+    }
+}
