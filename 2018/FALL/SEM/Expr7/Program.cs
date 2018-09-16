be463cdf9d1d3b3984db763c84fcbc276a91772f
Khaliullin Bulat <incidentalname636@gmail.com>
2018-09-16 23:16:06 +0300

    [add] Expr7

---
+++ 2018/FALL/SEM/Expr7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CS
+{
+    /// <summary>
+    /// Finding parallel and perpendicular vectors to given
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("A straight line parallel to given: y = " + k + " * x + " + (b+5));
+
+            double newK = -1 / Math.Tan(Math.Atan(k));
+
+            Console.WriteLine("A straight line perpendicular to given: y = " + newK + " * x + " + b);
+        
+            Console.ReadKey();
+        }
+    }
+}
