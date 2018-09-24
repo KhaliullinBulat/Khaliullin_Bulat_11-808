bb5188fa71fb53138ecb3f0354fe9575b516f376
Khaliullin Bulat <incidentalname636@gmail.com>
2018-09-24 08:39:05 +0300

    [add] Expr5

---
+++ 2018/FALL/SEM/Expr5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Practise
+{
+    /// <summary>
+    /// Counting amount of leap years 
+    /// </summary>
+    static class Program
+    {
+        private static void Main()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int c = b / 4 - a / 4;
+            if (a % 4 == 0 && b % 4 == 0)
+                c += 1;
+
+            int amountOfYears = c - (b / 100 - a / 100) + b / 400 - a / 400;
+
+            Console.WriteLine(amountOfYears);
+            Console.ReadKey();
+        }
+        
+    }
+}
