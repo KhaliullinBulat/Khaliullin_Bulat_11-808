5b8ba8d6486d14a1cbc042c9ec00ec4eb8d19640
Khaliullin Bulat <incidentalname636@gmail.com>
2018-09-26 12:36:43 +0300

    [add] Cond5

---
+++ 2018/FALL/SEM/Cond5/Practise.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Slide01
+{
+    /// <summary>
+    /// Khaliullin Bulat
+    /// Cond5
+    /// Finding minimal and maximum time for driving through the way
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            int L = int.Parse(Console.WriteLine());
+            int K = int.Parse(Console.WriteLine());
+            int H = int.Parse(Console.WriteLine());
+            int amount = L / K;
+            if (L % K == 0)
+                Console.WriteLine("Minimal and maximum times are equal: " + amount * H);
+            else
+            {
+                Console.WriteLine("Minimal time is " + amount * H);
+                Console.Write("Maximum time is " + (amount * H + H));
+            }
+            Console.ReadKey();
+        }
+    }
+}
