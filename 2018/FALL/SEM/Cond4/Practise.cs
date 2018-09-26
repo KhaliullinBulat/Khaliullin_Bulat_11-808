f73ccfd2265cf05e42e91a8c412de51ab63c9b64
Khaliullin Bulat <incidentalname636@gmail.com>
2018-09-26 13:09:35 +0300

    [add] Cond4

---
+++ 2018/FALL/SEM/Cond4/Practise.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Slide01
+{
+    /// <summary>
+    /// Khaliullin Bulat
+    /// Cond4
+    /// Finding intersection of two segments
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+            int D = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Math.Min(B, D) - Math.Max(A, C));
+
+            Console.ReadKey();
+        }
+    }
+}
