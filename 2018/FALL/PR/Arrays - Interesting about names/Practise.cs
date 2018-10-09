ea166701b4e15ee306e50dd784344011c4eca3ac
Khaliullin Bulat <incidentalname636@gmail.com>
2018-10-09 23:04:10 +0300

    [add] Interesting about names

---
+++ 2018/FALL/PR/Arrays - Interesting about names/Practise.cs
@@ -0,0 +1,40 @@
+﻿
+/*
+namespace Mazes
+{
+    public static class EmptyMazeTask
+    {
+        public static void MoveDownOrRight(Robot robot, int endPosition)
+        {
+            endPosition -= 2; // because there is also two walls in the top and the down sides
+            for (int beginPosition = 1; beginPosition < endPosition; beginPosition++)
+            {
+                if ()
+                robot.MoveTo(Direction.Down);
+                if ()
+                robot.MoveTo(Direction.Right);
+            }
+        }
+
+        public static void MoveDown(Robot robot, int heightEnd)
+        {
+            heightEnd = heightEnd - 2; // because there is also two walls in the top and the down sides
+            for (int heightBegin = 1; heightBegin < heightEnd; heightBegin++)
+                robot.MoveTo(Direction.Down);
+        }
+
+        public static void MoveRight(Robot robot, int widthEnd)
+        {
+            widthEnd = widthEnd - 2; // because there is also two walls in the left and the right sides
+            for (int widthBegin = 1; widthBegin < widthEnd; widthBegin++)
+                robot.MoveTo(Direction.Right);
+        }
+
+        public static void MoveOut(Robot robot, int width, int height)
+        {
+            MoveRight(robot, width);
+            MoveDown(robot, height);
+        }
+    }
+}
+*/
